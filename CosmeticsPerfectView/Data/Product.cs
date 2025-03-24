2edede003e86523c379ff25188fa205ba920d385
IvetAngelova06 <ivet.angelova650@edu1.pgzaimov.com>
2025-03-24 13:48:49 +0200

    ORDER controller

---
+++ CosmeticsPerfectView/Data/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CosmeticsPerfectView.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CategoriesId {  get; set; }
+        public Category Categories { get; set; }
+        public int ProductTypeId {  get; set; }
+        public ProductType ProductTypes { get; set; }
+        public string URLimage {  get; set; }
+
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Price { get; set; }
+
+
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal PromoPercent { get; set; }
+        public string Description { get; set; }
+        public DateTime DateRegister { get; set; } = DateTime.Now;
+        public ICollection<Order> Orders { get; set; }
+
+    }
+}
